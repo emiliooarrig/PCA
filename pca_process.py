f1e4e801a76be70a295bb387c7a645084caf16c5
Your Name <you@example.com>
2025-09-30 17:36:40 -0600

    Saving changes

---
+++ pca_process.py
@@ -0,0 +1,12 @@
+#En este programa se procesan las imagenes
+#para poder ser usadas en test o en train
+
+#Funciones necesarias:
+
+#pro_image procesa imagenes a vectores 
+# (los pasa a escala de grises, reduce su 
+# tamaño y lo pasa a vectores)
+
+#vect_process toma los vectores de 
+# una persona y hace el analisis de 
+# componentes
---
+++ pca_train.py
@@ -0,0 +1,11 @@
+# Este programa es para entrenar al 
+# modelo que será guardado en el folder 
+# de "models".
+
+#Funciones necesarias:
+
+# importar y usar pca_process para procesar 
+# las imagenes y conseguir los vectores
+
+# train_model  el resultado es guardado 
+# en "models"
